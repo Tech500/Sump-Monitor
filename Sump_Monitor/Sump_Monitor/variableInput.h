f6245f3e2e374b0c6052f67203cbd0bf68a3d634
William Lucid <ab9nq.william@gmail.com>
2026-04-13 23:27:23 -0400

    Update variableInput.h

--- Sump_Monitor/Sump_Monitor/variableInput.h
+++ Sump_Monitor/Sump_Monitor/variableInput.h
@@ -5,21 +5,20 @@
 // 
 
 // Replace with your network details  
-//const char * host  = "esp32";
+const char * host  = "Sump-Monitor";
 
-// Replace with your network details
-const char * ssid = "R2D2";
-const char * password = "Sky7388500";
+#define SSID       ""
+#define PASS       ""
 
 //Settings pertain to NTP
 const int udpPort = 1337;
 //NTP Time Servers
-const char * udpAddress1 = "us.pool.ntp.org";
-const char * udpAddress2 = "time.nist.gov";
+const char * NTP0 = "us.pool.ntp.org";
+const char * NTP1 = "time.nist.gov";
 
 //publicIP accessiable over Internet with Port Forwarding; know the risks!!!
 //WAN IP Address.  Or use LAN IP Address --same as server ip; no Internet access. 
-#define publicIP "https://sump-monitor.tailb986d2.ts.net/"  //Part of href link for "GET" requests
+#define publicIP "https://sump-monitor.xxxxxxxxxx.ts.net/"  //Part of href link for "GET" requests
 
 //Find your public ipAddress:  https://whatismyipaddress.com/
 
@@ -31,24 +30,33 @@ String ip1String = "hostipaddress";  //Host ip address
 
 int PORT = 80;  //Web Server port
 
+// ===================================================
+// Email sender credentials
+// ===================================================
+#define SENDER_EMAIL   ""       // sending Gmail account  <---- use a seperate Gmail account; not your main Gmail account set filter!
+#define SENDER_PASS    ""            // Gmail app password
+
+// ===================================================
+// Alert destinations
+// ===================================================
+#define ALERT_EMAIL    ""         // Larry's dedicated sump Gmail
+#define ALERT_SMS      ""        // Larry's T-Mobile SMS gateway
+#define ALERT_EMAIL2   ""       // your monitoring copy
+
+#define ALERT_TAG  "SUMP"
+
 //Graphing requires "FREE" "ThingSpeak.com" account..  
 //Enter "ThingSpeak.com" data here....
 //Example data; enter yout account data..
 unsigned long int myChannelNumber = 123456; 
 const char * myWriteAPIKey = "EE2345";
 
-//Server settings --all internal LAN addresses
-#define ip {192,168,12,22}
-#define subnet {255,255,255,0}
-#define gateway {192,168,12,1}
-#define dns {192,168,12,1}
-
 //webInterface --send Data to Domain, hosted web site
 const char * sendData = "your domaindestination and filename for data from webInterface function";
 
 //FTP Credentials
-//const char * ftpUser = "username";
-//const char * ftpPassword = "password";
+const char * ftpUser = "sump";
+const char * ftpPassword = "sumpone";
  
 //Restricted Access
 const char* Restricted = "/Restricted";  //Can be any filename.  
@@ -69,3 +77,12 @@ const char* http_password = "admin";
 // xx.xx.xx.xx being publicIP and yyyy being PORT.
 //
 ///////////////////////////////////////////////////////
+
+// ===================================================
+// Sinric Pro credentials
+// ===================================================
+#define APP_KEY    ""
+#define APP_SECRET ""
+#define HIGH_WATER_ID  "69dd5cd5a221de787934078c"
+#define FLOODING_ID    "69dd605852800e7ce3667a56"
+#define ALL_CLEAR_ID   "69dd612d52800e7ce3667ab4"
