828f521541fe90e65633b04610721f3e4d65e2f7
William Lucid <ab9nq.william@gmail.com>
2026-04-20 18:49:38 -0400

    Updated 04/20/2026 @ 18:49 EDT

---
+++ Sump_Monitor/index2.h
@@ -0,0 +1,21 @@
+//index2.h
+const char HTML2[] PROGMEM = R"====(
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>SDBrowse</title>
+</head>
+
+<body>
+    <h2>Collected Observations
+    <br>
+    <br> %URLLINK% 
+    <br>
+    <br> 
+    <a href="/SdBrowse">Filereader</a><br> 
+    <a href="/sump">Main Menu</a></h2><br>
+</body>
+
+</html>
+)====";
